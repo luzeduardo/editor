0507efcf58616a49f45459e5b180e883d32f33cc
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2024-05-07 07:00:50 +0100

    adds tab keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,4 +15,6 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
-keymap.set("n", "te", "tabedit")
+keymap.set("n", "te", "tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
