3fe63f85fbd6ddb784b4f0c19362aaf7bfb336fb
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2024-05-07 07:10:29 +0100

    adds window resize and diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,3 +28,14 @@ keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+--diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
